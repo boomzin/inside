bbb6571e527f9c781f9b9ca2c7fdb1b2d467827f
boomzinHoumePC <boomzin@gmail.com>
2021-11-28 17:05:24 +0300

    Initial commit

---
+++ src/main/java/com/example/inside/InsideApplication.java
@@ -0,0 +1,13 @@
+package com.example.inside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsideApplication.class, args);
+    }
+
+}
